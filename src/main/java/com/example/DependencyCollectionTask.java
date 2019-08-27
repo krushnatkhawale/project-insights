3b963e44c16e75cef4dc838b8abc8a3f1de0e8f3
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-08-27 23:45:20 +0100

    Second cut: enabling jar path filtering

--- src/main/java/com/example/DependencyCollectionTask.java
+++ src/main/java/com/example/DependencyCollectionTask.java
@@ -1,47 +1,40 @@
 package com.example;
 
 import org.gradle.api.DefaultTask;
-import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.tasks.TaskAction;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class DependencyCollectionTask extends DefaultTask {
 
-    String appName;
-    String groupName;
-    List<Dependency> dependencies;
-
-    public String getAppName() {
-        return appName;
-    }
-
-    public void setAppName(String appName) {
-        this.appName = appName;
-    }
-
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
-
-    public List<Dependency> getDependencies() {
-        return dependencies;
-    }
-
-    public void setDependencies(List<Dependency> dependencies) {
-        this.dependencies = dependencies;
-    }
-
-
     @TaskAction
     void postInfo() {
-        System.out.println("Posting dependency info");
-        System.out.println("Application name: " + appName);
-        System.out.println("Group: " + groupName);
-        System.out.println("Posting to: url");
+        System.out.println("  Task is being executed!");
+
+        Project project = getProject();
+
+        System.out.println("  Group: " + getGroup());
+        System.out.println("  Project: " + project);
+        System.out.println("  Service: " + getServices());
+
+        List<Dependency> collect = project.getConfigurations()
+                .stream()
+                .filter(config -> config.getName().contains("compile"))
+                .map(Configuration::resolve)
+                .flatMap(Collection::stream)
+                .map(Dependency::toDependecy)
+                .sorted((Comparator.comparing(Dependency::getGroup)))
+                .collect(Collectors.toList());
+
+        collect.forEach(cd -> {
+            System.out.println("   \\ d: " + cd);
+        });
+
+        System.out.println("Test - 1");
     }
 }
