eb90d0c11c49855817ded96e365ece895cfdcf0a
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-08-27 23:45:11 +0100

    Second cut: enabling jar path filtering

--- src/main/java/com/example/DependencyCollectionPlugin.java
+++ src/main/java/com/example/DependencyCollectionPlugin.java
@@ -2,15 +2,26 @@ package com.example;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
 
 public class DependencyCollectionPlugin implements Plugin<Project> {
 
     public static final String TASK_NAME = "dependency-collection";
+    public static final String BUILD_TASK = "build";
     private DefaultAction defaultAction = new DefaultAction();
 
     @Override
     public void apply(Project project) {
-        project.getTasks()
+        System.out.println("  Plugin is being applied!");
+
+        Task buildTask = project.getTasks()
+                .findByName(BUILD_TASK);
+
+        DependencyCollectionTask thisTask = project.getTasks()
                 .create(TASK_NAME, DependencyCollectionTask.class, defaultAction);
+
+        buildTask.dependsOn(thisTask);
+
+
     }
 }
