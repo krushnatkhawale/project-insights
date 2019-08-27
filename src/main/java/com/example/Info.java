2844472097cd52526ea3301f9e96d5fefa067cc0
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2019-08-27 23:45:25 +0100

    Second cut: enabling jar path filtering

---
+++ src/main/java/com/example/Info.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.List;
+
+public class Info {
+
+    String appName;
+    String groupName;
+    List<Dependency> dependencies;
+
+    public String getAppName() {
+        return appName;
+    }
+
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public List<Dependency> getDependencies() {
+        return dependencies;
+    }
+
+    public void setDependencies(List<Dependency> dependencies) {
+        this.dependencies = dependencies;
+    }
+
+}
